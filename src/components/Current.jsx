2cb92a8620a1bc33f899465ce8f88a76753556f8
gazibarad <gazibaradino@outlook.com>
2022-05-09 16:28:25 +0200

    Fixing responsiveness issues

--- src/components/Current.jsx
+++ src/components/Current.jsx
@@ -9,7 +9,7 @@ const Current = (props) => {
         <h1 id="current-temperature">{`${Math.round(
           props.weatherData.main.temp
         )}°C`}</h1>
-        <h1 id="current-desc">{props.weatherData.weather[0].description}</h1>
+        <h1 id="current-desc">{props.weatherData.weather[0].main}</h1>
       </div>
     </AnimatedContent>
   );
